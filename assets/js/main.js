78eeecc8222ac2f92b1048fab4ac1016d42c09d2
btzmdd <>
2024-04-04 22:03:44 +0800

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navToggle){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== SWIPER SNEAKERS ===============*/
+let swiperImages = new Swiper('.home_swiper', {
+    loop: true,
+    spaceBetween: 64,
+    grabCursor: true,
+    centeredSlides: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+  
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+})
+
+let swiperTitles = new Swiper('.home_titles', {
+    loop: true,
+    spaceBetween: 64,
+    grabCursor: true,
+    centeredSlides: true,
+})
+
+swiperImages.controller.control = swiperTitles
+swiperTitles.controller.control = swiperImages
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')                      
+}
+window.addEventListener('scroll', bgHeader)
